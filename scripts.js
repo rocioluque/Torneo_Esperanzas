1074c47e21b750b19f6fe357784fe364e0a9a8f3
rocioluque <rocioluq4@gmail.com>
2024-03-08 16:38:48 -0300

    w

--- scripts.js
+++ scripts.js
@@ -1,26 +1,48 @@
 function obtenerFechaTexto() {
-    // Creo un objeto fecha
-    let fecha = new Date();
+  // Creo un objeto fecha
+  let fecha = new Date();
 
-    // Creo un array con los días de la semana en texto
-    let diasSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-    let diaSemanaTexto = diasSemana[fecha.getDay()];
+  // Creo un array con los días de la semana en texto
+  let diasSemana = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+  let diaSemanaTexto = diasSemana[fecha.getDay()];
 
-    // Creo otro array con los meses en texto
-    let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-    let mesTexto = meses[fecha.getMonth()];
+  // Creo otro array con los meses en texto
+  let meses = [
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre",
+  ];
+  let mesTexto = meses[fecha.getMonth()];
 
-    // Obtengo el día
-    let dia = fecha.getDate();
+  // Obtengo el día
+  let dia = fecha.getDate();
 
-    // Obtengo el año
-    let año = fecha.getFullYear();
+  // Obtengo el año
+  let año = fecha.getFullYear();
 
-    // Creo la cadena de texto con la fecha
-    let fechaTexto = diaSemanaTexto + ', ' + dia + ' de ' + mesTexto + ' de ' + año;
+  // Creo la cadena de texto con la fecha
+  let fechaTexto =
+    diaSemanaTexto + ", " + dia + " de " + mesTexto + " de " + año;
 
-    // Devuelvo la fecha en formato de texto
-    return fechaTexto;
+  // Devuelvo la fecha en formato de texto
+  return fechaTexto;
 }
 
 // Obtengo el elemento <p> por su ID
--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -23,6 +23,23 @@ router.post("/", async (req, res) => {
   }
 });
 
+app.post('/usuarios', (req, res) => {
+  const accessToken = req.body.accessToken;
+
+  // Verificar el token
+  jwt.verify(accessToken, 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlV0SmRjR2E5LXpGMDJyZXpvMWhoMiJ9.eyJpc3MiOiJodHRwczovL2Rldi1sbDZyY21xM2U1aW40ZXIzLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJqdTlXeFpMbnVpc3BNVjhXMDN0Qk1Tc21QSW1tbkx6SkBjbGllbnRzIiwiYXVkIjoiaHR0cDovb3JnYW5pemFjaW9uZXNwZXJhbnphcy5uZXRsaWZ5LmFwcC9hcGkvdXN1YXJpb3MiLCJpYXQiOjE3MDk5MjUxNjgsImV4cCI6MTcxMDAxMTU2OCwiYXpwIjoianU5V3haTG51aXNwTVY4VzAzdEJNU3NtUEltbW5MekoiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.G2T-HKp1Fu-WkfRe_lSuGHz0-_MVp4VzjhHXUbrWv_bOLcsTLjhUG4c9tqqVnd3scFbE6bojduglehEdLOGmSeU6wEa-9hAS-YSRSi51Kg3fyQnx1gxeJnPi59hbKqTpg6mnLPpXrm-hRK_5TipLeS776vpmb9lyZBFxEmvMC7upsMCwxLtQXo01tIQ-mLTIqCeSQ256sH6Ghg2lp-aTkFibG8HPr9xXdF1eP32rPYDBCb-LfUQyGx53fV24FXrBfJOxryRUGNpLWy3VnuGeZyvDYg-Cf4D1NNXmTDu7Oh8vd7MMXihDfTExo3YI-ZrqxnDkkHiOG_oyCc8Q2ShbsA', (err, decoded) => {
+      if (err) {
+          // Token no válido
+          return res.status(401).json({ error: 'Unauthorized' });
+      }
+
+      // El token es válido, puedes continuar con el registro del usuario
+      // Accede a los datos del usuario a través de 'decoded'
+
+      res.status(200).json({ message: 'Registro exitoso' });
+  });
+});
+
 // Ruta para actualizar un Usuario existente
 router.put("/:id", async (req, res) => {
   try {
