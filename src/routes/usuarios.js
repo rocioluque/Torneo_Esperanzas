cf4a73bbc43136c128879db8ac19b9b0bf8b485d
rocioluque <rocioluq4@gmail.com>
2024-03-08 17:06:37 -0300

    w

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
 });
 
 // Ruta para crear un nuevo Usuario
-router.post("/", async (req, res) => {
+router.post("/usuarios", async (req, res) => {
   try {
     const nuevoUsuario = new Usuario(req.body);
     await nuevoUsuario.save();
